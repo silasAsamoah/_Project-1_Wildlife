6edac427ee095cde9f81be93c03c01b1c040c70b
silasAsamoah <three.chapter.me@gmail.com>
2020-08-14 07:09:13 +0000

    Initial Commit - static responsive  page done

---
+++ js/page-load.js
@@ -0,0 +1,20 @@
+//wait for window load
+// const loader = document.querySelector('.loading');
+// const homePage = document.querySelector('.landing-page');
+
+// function init(){
+//     setTimeout(()=> {
+//         loader.style.opacity=0;
+//         loader.style.display='none';
+
+//         homePage.style.display='grid';
+//         setTimeout(()=>(homePage.style.opacity=1), 100);   //content fades in 
+//     },2000);
+// }
+
+// init();
+
+window.addEventListener('load',()=> {
+    const preloader = document.querySelector('.loading');
+    preloader.classList.add('load-finish');
+});
