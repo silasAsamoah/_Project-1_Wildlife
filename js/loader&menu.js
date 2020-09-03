23ba7c03acff02243a4b57293bce8598f03a1bc2
silasAsamoah <three.chapter.me@gmail.com>
2020-09-03 22:18:12 +0000

    added menu nav for mobile devices

--- js/loader&menu.js
+++ js/loader&menu.js
@@ -14,8 +14,29 @@
 
 // init();
 
+//FOR PAGE RELOAD
 //reload based on page load
 window.addEventListener('load',()=> {
     const preloader = document.querySelector('.loading');
     preloader.classList.add('load-finish');
 });
+
+//FOR MENU
+var menu=document.getElementById('menu_nav');
+//show menu content on menuclick
+//and add styling class
+function show(){
+    menu.style.display='block';
+    menu.classList.add('menu_content');
+
+
+    /*hide menu content onselection or outside click
+    */
+    window.addEventListener('mouseup',function(event){
+        if(event.target != menu){
+            menu.style.display="none";
+            menu.classList.remove('menu_content');
+        }
+    });  
+}
+
